bfb7ed455a557aae1557b48dd8f10c39f29020cc
spartan0614 <62442joss@gmail.com>
2017-12-20 22:31:57 -0600

    Nuevo

--- build-Practica1-Desktop_Qt_5_9_0_MinGW_32bit-Debug/debug/moc_aereopuerto.cpp
+++ build-Practica1-Desktop_Qt_5_9_0_MinGW_32bit-Debug/debug/moc_aereopuerto.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Practica1/aereopuerto.h"
+#include "../../../../../../Downloads/Practica1/aereopuerto.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
